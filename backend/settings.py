5ef567bc3d8f8f19b23ea5a4190e9a8eef5d682e
Rafał Hrabia <rafixese@gmail.com>
2024-11-03 17:42:03 +0100

    Added pagination

--- backend/settings.py
+++ backend/settings.py
@@ -114,6 +114,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',  # Optional, for testing with credentials
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 AUTHENTICATION_BACKENDS = [
